46788da85de9de3f993bb2adebeb5d951dd67427
nadineprofesseur <stamandnadine@cgmatane.qc.ca>
2022-09-20 08:28:37 -0400

    Factorisation des download de json et xml

---
+++ src/donnee/ServiceDAO.java
@@ -0,0 +1,27 @@
+package donnee;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+
+public class ServiceDAO {
+
+	public String telechargerDonnees(String hyperlien)
+	{
+		String xml = null;
+		try {
+			URL url = new URL(hyperlien);
+			InputStream flux = url.openConnection().getInputStream();
+			Scanner lecteur = new Scanner(flux);
+			
+			lecteur.useDelimiter("\\A");
+			xml = lecteur.next();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if(xml == null) return null;
+		return xml;
+	}
+	
+}
